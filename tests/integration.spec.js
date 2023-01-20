a97927f3c93f02928b22f9aa16527deaeb28cc13
Ivo Genev <ivo.genev@and.digital>
2023-01-20 17:08:08 +0000

    feat: add support for try statements

--- tests/integration.spec.js
+++ tests/integration.spec.js
@@ -461,4 +461,91 @@ describe('needless-await-synchronisation', () => {
       ]
     });
   });
+
+  describe('handles awaits in try-catch blocks', () => {
+    ruleTester.run("needless-await-synchronisation", rule, {
+      valid: [
+        `
+        const f = async () => {
+          const { data } = await fa();
+          try {
+            const b = await fb(data)
+          } catch (e) {}
+        }
+        `,
+        `
+        const f = async () => {
+          const { data } = await fa();
+          try {
+            const b = fb(data)
+          } catch (e) {
+            await fc(data);
+          }
+        }
+        `,
+        `
+        const f = async () => {
+          const { data } = await fa();
+          try {
+            const b = fb(data)
+          } catch (e) {
+            const b = fc(data);
+          } finally {
+            await fd(data)
+          }
+        }
+        `,
+      ],
+      invalid: [
+        {
+          code: `
+          const f = async () => {
+            const { data } = await fa();
+            try {
+              const b = await fb()
+            } catch (e) {}
+          }
+          `,
+          errors: [
+            { message: "Unneeded synchronisation, please use Promise.all()" },
+            { message: "Unneeded synchronisation, please use Promise.all()" }
+          ],
+        },
+        {
+          code: `
+          const f = async () => {
+            const { data } = await fa();
+            try {
+              const b = fb();
+            } catch (e) {
+              const c = await fc();
+            }
+          }
+          `,
+          errors: [
+            { message: "Unneeded synchronisation, please use Promise.all()" },
+            { message: "Unneeded synchronisation, please use Promise.all()" }
+          ],
+        },
+        {
+          code: `
+          const f = async () => {
+            const { data } = await fa();
+            try {
+              const b = fb();
+            } catch (e) {
+              const c = fc();
+            } finally {
+              await fd();
+            }
+          }
+          `,
+          errors: [
+            { message: "Unneeded synchronisation, please use Promise.all()" },
+            { message: "Unneeded synchronisation, please use Promise.all()" }
+          ],
+        },
+      ]
+    });
+  });
 });
